182bf4ad0211bf68d4cffeb6f63eb2c20ce640ec
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-04-02 15:00:05 +0200

    Update About page

--- src/lib/Section/About.svelte
+++ src/lib/Section/About.svelte
@@ -5,8 +5,8 @@
 <Section id="about">
   <div class="center">
     <div>
-      <h1 class="header">About ArcOS</h1>
-      <h3 class="subheader">What even is it?</h3>
+      <h1 class="header">About</h1>
+      <h3 class="subheader">What even is ArcOS?</h3>
       <p class="context">
         ArcOS is an Operating System Environment built using web technologies.
         With ArcOS we aim to create the best OS experience you can get using web
@@ -14,6 +14,7 @@
         count, ArcOS is the biggest project I have ever taken on, and there are
         few things quite like it.
       </p>
+      <a href="https://github.com/IzK-ArcOS" class="github">Visit our GitHub</a>
     </div>
   </div>
 </Section>
@@ -44,6 +45,27 @@
 
   p.context {
     max-width: 700px;
+    margin-bottom: 50px;
+  }
+  a.github {
+    display: block;
+    margin: 0 auto;
+    color: var(--fg);
+    background-color: #222;
+    width: fit-content;
+    padding: 10px 15px;
+    border-radius: 5px;
+    text-decoration: none;
+    font-size: 14px;
+    line-height: 18px;
+    display: flex;
+  }
+
+  a.github::after {
+    content: "launch";
+    font-family: "Material Icons Round";
+    font-size: 18px;
+    margin-left: 10px;
   }
 
   @media screen and (max-width: 800px) {
@@ -60,6 +82,7 @@
     p.context {
       max-width: unset;
       padding: 20px;
+      margin-bottom: 10px;
     }
   }
 </style>
